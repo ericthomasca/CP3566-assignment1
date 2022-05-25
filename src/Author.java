c127034db0fa20d510d8d68ad0f01d6276728d0f
Eric Thomas <ericthomas@tuta.io>
2022-05-25 15:46:40 -0230

    add class author

--- src/Author.java
+++ src/Author.java
@@ -1,12 +1,14 @@
-import java.util.List;
-
 // Create an“Author” class which will represent the authors from the “authors” table
 // in the book's database. Each author object will have a list of the books they have
 // written–a private attribute List<Book> bookList.(Make sure to populate the bookList
 // attribute when loading the objects from the database.)
 
+import java.util.List;
+
 /**
  * A book author
+ *
+ * @author Eric Thomas
  */
 public class Author {
     private int id;
@@ -16,9 +18,10 @@ public class Author {
 
     /**
      * A book author
-     * @param id author id
+     *
+     * @param id        author id
      * @param firstName author's first name
-     * @param lastName author's last name
+     * @param lastName  author's last name
      */
     public Author(int id, String firstName, String lastName) {
         this.id = id;
@@ -28,6 +31,7 @@ public class Author {
 
     /**
      * Returns author id
+     *
      * @return author id
      */
     public int getId() {
@@ -36,6 +40,7 @@ public class Author {
 
     /**
      * Sets author id
+     *
      * @param id author id
      */
     public void setId(int id) {
@@ -44,6 +49,7 @@ public class Author {
 
     /**
      * Returns author's first name
+     *
      * @return author's first name
      */
     public String getFirstName() {
@@ -52,6 +58,7 @@ public class Author {
 
     /**
      * Sets author's first name
+     *
      * @param firstName author's first name
      */
     public void setFirstName(String firstName) {
@@ -60,6 +67,7 @@ public class Author {
 
     /**
      * Returns author's last name
+     *
      * @return author's last name
      */
     public String getLastName() {
@@ -68,6 +76,7 @@ public class Author {
 
     /**
      * Sets authors last name
+     *
      * @param lastName author's last name
      */
     public void setLastName(String lastName) {
@@ -76,6 +85,7 @@ public class Author {
 
     /**
      * Returns list of books from the author
+     *
      * @return list of books from the author
      */
     public List<Book> getBookList() {
@@ -84,6 +94,7 @@ public class Author {
 
     /**
      * Sets the list of books from the author
+     *
      * @param bookList list of books from the author
      */
     public void setBookList(List<Book> bookList) {
