869abded8234f16032ab8e9140ea2131c9fd01e4
Eric Thomas <ericthomas@tuta.io>
2022-05-25 15:42:24 -0230

    complete Author class

--- src/Author.java
+++ src/Author.java
@@ -1,27 +1,96 @@
+import java.util.List;
+
 // Create an“Author” class which will represent the authors from the “authors” table
 // in the books database. Each author object will have a list of the books they have
 // written–a private attribute List<Book> bookList.(Make sure to populate the bookList
 // attribute when loading the objects from the database.)
+
+/**
+ * A book author
+ */
 public class Author {
-    private int authorID;
+    private int id;
     private String firstName;
     private String lastName;
+    private List<Book> bookList;
 
-    public Author(int authorID, String firstName, String lastName) {
-        this.authorID = authorID;
+    /**
+     * A book author
+     * @param id author id
+     * @param firstName author's first name
+     * @param lastName author's last name
+     */
+    public Author(int id, String firstName, String lastName) {
+        this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
     }
 
-    public int getAuthorID() {
-        return authorID;
+    /**
+     * Returns author id
+     * @return author id
+     */
+    public int getId() {
+        return id;
     }
 
+    /**
+     * Sets author id
+     * @param id author id
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * Returns author's first name
+     * @return author's first name
+     */
     public String getFirstName() {
         return firstName;
     }
 
+    /**
+     * Sets author's first name
+     * @param firstName author's first name
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    /**
+     * Returns author's last name
+     * @return author's last name
+     */
     public String getLastName() {
         return lastName;
     }
+
+    /**
+     * Sets authors last name
+     * @param lastName author's last name
+     */
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    /**
+     * Returns list of books from the author
+     * @return list of books from the author
+     */
+    public List<Book> getBookList() {
+        return bookList;
+    }
+
+    /**
+     * Sets the list of books from the author
+     * @param bookList list of books from the author
+     */
+    public void setBookList(List<Book> bookList) {
+        this.bookList = bookList;
+    }
 }
+
+
+
+
