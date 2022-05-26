0385583641432e2031ce6a30e944ebe0fecab6af
Eric Thomas <ericthomas@tuta.io>
2022-05-25 23:34:03 -0230

    Book javadocs

--- src/Book.java
+++ src/Book.java
@@ -4,14 +4,25 @@
 
 import java.util.List;
 
+/**
+ * A book
+ *
+ * @author Eric Thomas
+ */
 public class Book {
-
     private String ISBN;
     private String title;
     private int edition;
     private String copyright;
     private List<Author> authorList;
 
+    /**
+     * A book
+     * @param ISBN ISBN
+     * @param title book title
+     * @param edition book edition
+     * @param copyright book copyright
+     */
     public Book(String ISBN, String title, int edition, String copyright) {
         this.ISBN = ISBN;
         this.title = title;
@@ -19,42 +30,92 @@ public class Book {
         this.copyright = copyright;
     }
 
+    /**
+     * Returns ISBN
+     *
+     * @return ISBN
+     */
     public String getISBN() {
         return ISBN;
     }
 
+    /**
+     * Set ISBN
+     *
+     * @param ISBN ISBN
+     */
     public void setISBN(String ISBN) {
         this.ISBN = ISBN;
     }
 
+    /**
+     * Returns title of book
+     *
+     * @return book title
+     */
     public String getTitle() {
         return title;
     }
 
+    /**
+     * Set book title
+     *
+     * @param title book title
+     */
     public void setTitle(String title) {
         this.title = title;
     }
 
+    /**
+     * Returns book edition
+     *
+     * @return book edition
+     */
     public int getEdition() {
         return edition;
     }
 
+    /**
+     * Set book edition
+     *
+     * @param edition book edition
+     */
     public void setEdition(int edition) {
         this.edition = edition;
     }
 
+    /**
+     * Returns copyright
+     *
+     * @return copyright
+     */
     public String getCopyright() {
         return copyright;
     }
 
+    /**
+     * Set copyright
+     *
+     * @param copyright copyright
+     */
     public void setCopyright(String copyright) {
         this.copyright = copyright;
     }
 
+    /**
+     * Returns list of authors
+     *
+     * @return list of authors
+     */
     public List<Author> getAuthorList() {
         return authorList;
     }
 
+    /**
+     * Sets list of authors
+     *
+     * @param authorList list of authors
+     */
     public void setAuthorList(List<Author> authorList) {
         this.authorList = authorList;
     }
