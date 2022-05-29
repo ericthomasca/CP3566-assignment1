13cfa05e42c05399d1d07249b4d063bf93b1c8cb
Eric Thomas <ericthomas@tuta.io>
2022-05-29 21:29:34 -0230

    complete BookDatabaseManager methods

--- src/BookDatabaseManager.java
+++ src/BookDatabaseManager.java
@@ -1,9 +1,3 @@
-// Create a “BookDatabaseManager” class which will handle all database connections
-// and queries as well as store lists of Books and Authors to be used by the main
-// application. It is recommended that you do the following:
-// •Stores the database URL and login as static private members. Note, any time
-//  you use a database connection ensure that you close the connection when
-//  finished –don’t allow connections to persist.
 // •Create a private loadDatabase method that coordinates the queries for both
 //  the books and authors and their relationships. Note: you can use other
 //  private methods to simplify the code. (Open and close the database connection
@@ -20,7 +14,6 @@
 
 import java.sql.*;
 import java.util.List;
-import java.util.Objects;
 
 public class BookDatabaseManager {
     private final String DATABASE_URL = "jdbc:mariadb://localhost:3306/books";
@@ -28,8 +21,25 @@ public class BookDatabaseManager {
     private final String PASS = "Q5obBNgUy4";
     private List<Book> bookList;
     private List<Author> authorList;
-
     public BookDatabaseManager() {
+    }
+
+    public Connection loadDatabase() throws SQLException {
+        return DriverManager.getConnection(DATABASE_URL, USER, PASS);
+    }
+
+    public ResultSet loadBooks() throws SQLException {
+        Connection connection = loadDatabase();
+        Statement statement = connection.createStatement();
+        String sqlQuery = "SELECT * from titles";
+        return statement.executeQuery(sqlQuery);
+    }
+
+    public ResultSet loadAuthors() throws SQLException {
+        Connection connection = loadDatabase();
+        Statement statement = connection.createStatement();
+        String sqlQuery = "SELECT * from authors";
+        return statement.executeQuery(sqlQuery);
     }
 
     public List<Book> getBookList() throws SQLException {
@@ -73,6 +83,7 @@ public class BookDatabaseManager {
         String sqlQuery = "INSERT INTO titles (isbn, title, editionNumber, copyright) " +
                 "VALUES (" + ISBN + ", " + title + ", " + edition + ", " + copyright + ");";
         statement.executeQuery(sqlQuery);
+        connection.close();
     }
 
     public void addNewAuthor(Author author) throws SQLException {
@@ -87,23 +98,6 @@ public class BookDatabaseManager {
                 "VALUES (" + authorID + ", " + firstName + ", " + lastName + ");";
 
         statement.executeQuery(sqlQuery);
-    }
-
-    private Connection loadDatabase() throws SQLException {
-        return DriverManager.getConnection(DATABASE_URL, USER, PASS);
-    }
-
-    private ResultSet loadBooks() throws SQLException {
-        Connection connection = loadDatabase();
-        Statement statement = connection.createStatement();
-        String sqlQuery = "SELECT * from titles";
-        return statement.executeQuery(sqlQuery);
-    }
-
-    private ResultSet loadAuthors() throws SQLException {
-        Connection connection = loadDatabase();
-        Statement statement = connection.createStatement();
-        String sqlQuery = "SELECT * from authors";
-        return statement.executeQuery(sqlQuery);
+        connection.close();
     }
 }
